a3ca2e81b90568874a9111826c10775c901f6061
Ed <harriss.edward24@gmail.com>
2021-03-20 19:26:38 +0000

    Changed bits to 22

--- Cordic/cordic.v
+++ Cordic/cordic.v
@@ -7,56 +7,54 @@ module cordic(
 );
 
 input clk, reset, start;
-input [31:0] angle;
-output [31:0] cos_out;
+input [21:0] angle;
+output [21:0] cos_out;
 
 reg state, state_next;
 reg [3:0] i, i_next;  //iterator
-reg [31:0] e_i, x, y, z, x_next, y_next, z_next; //angle of iteration plus others...
+reg [21:0] e_i, x, y, z, x_next, y_next, z_next; //angle of iteration plus others...
 
-wire [31:0] cos_out;
+wire [21:0] cos_out, x_shifted, y_shifted;
 wire d;
-wire [31:0] x_shifted;
-wire [31:0] y_shifted;
 
 assign cos_out = x;
-assign d = z[31];
+assign d = z[21];
 assign x_shifted = x >> i;
 assign y_shifted = y >> i;
 
 always @(i) begin //angle to be used in calc
     case(i)
-        4'd0 : e_i <= 32'h3243f6a9;
-        4'd1 : e_i <= 32'h1dac6705;
-        4'd2 : e_i <= 32'h0fadbafd;
-        4'd3 : e_i <= 32'h07f56ea7;
-        4'd4 : e_i <= 32'h03feab77;
-        4'd5 : e_i <= 32'h01ffd55c;
-        4'd6 : e_i <= 32'h00fffaab;
-        4'd7 : e_i <= 32'h007fff55;
-        4'd8 : e_i <= 32'h003fffeb;
-        4'd9 : e_i <= 32'h001ffffd;
-        4'd10 : e_i <= 32'h00100000;
-        4'd11 : e_i <= 32'h00080000;
-        4'd12 : e_i <= 32'h00040000;
-        4'd13 : e_i <= 32'h00020000;
-        4'd14 : e_i <= 32'h00010000;
-        4'd15 : e_i <= 32'h00008000;
+        4'd0 : e_i <= 22'b11001001000011111101;
+        4'd1 : e_i <= 22'b01110110101100011001;
+        4'd2 : e_i <= 22'b00111110101101101110;
+        4'd3 : e_i <= 22'b00011111110101011011;
+        4'd4 : e_i <= 22'b00001111111110101010;
+        4'd5 : e_i <= 22'b00000111111111110101;
+        4'd6 : e_i <= 22'b00000011111111111110;
+        4'd7 : e_i <= 22'b00000001111111111111;
+        4'd8 : e_i <= 22'b00000000111111111111;
+        4'd9 : e_i <= 22'b00000000011111111111;
+        4'd10 : e_i <= 22'b00000000010000000000;
+        4'd11 : e_i <= 22'b00000000001000000000;
+        4'd12 : e_i <= 22'b00000000000100000000;
+        4'd13 : e_i <= 22'b00000000000010000000;
+        4'd14 : e_i <= 22'b00000000000001000000;
+        4'd15 : e_i <= 22'b00000000000000100000;
     endcase
 end
 
 always@(posedge clk) begin
     if (start) begin   //initial values
         i <= 4'd0;
-        x <= 32'h26dd3b6a;
-        y <= 32'd0;
+        x <= 22'b10011011011101001110;
+        y <= 22'd0;
         z <= angle;
         state <= 1'b1;
     end
 	 else if (reset) begin
         i <= 4'd0;
-        x <= 32'h26dd3b6a;
-        y <= 32'd0;
+        x <= 22'b10011011011101001110;
+        y <= 22'd0;
 		  z <= angle;
 	 end
     else begin        //updateing values
