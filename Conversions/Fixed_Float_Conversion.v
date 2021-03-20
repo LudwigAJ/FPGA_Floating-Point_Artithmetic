c835425c3c2ea5e27f1a326f0adb01824f4fa6c4
Ludwig <ludwigalfred@gmail.com>
2021-03-20 22:58:59 +0000

    Add files via upload
    Conversions between Floating point IEEE-754 and 22 -bit Fixed point with 1 sign bit, 1 integer, and 20 fractional bits.

---
+++ Conversions/Fixed_Float_Conversion.v
@@ -0,0 +1,45 @@
+// Fixed point 22 bit to single precision 32 bit (IEEE-754)
+// 22-bit fixed point : 1 sign bit + 1 integer bit + 20 fractional bits.
+
+module fixed_to_float(
+    data,
+    result,
+    clk
+    );
+
+    input clk;
+    input [21:0] data;
+    output reg [31:0] result;
+
+    wire sign_fixed;
+    wire [20:0] fixed_val;
+
+    reg sign_float;
+    reg [7:0] exp_float;
+    reg [23:0] mant_float;
+    reg [4:0] counter;
+
+    assign {sign_fixed, fixed_val[20:0]} = data;
+
+    always @ (posedge clk) begin
+
+        if (fixed_val == 21'd0) begin
+            result = 32'b0;
+        end
+        else begin
+            sign_float = sign_fixed;
+            exp_float = 8'd127; // i.e. set to 0 + bias = 127.
+            mant_float[23:3] = fixed_val[20:0];
+
+            counter = 5'b0;
+
+            while (~mant_float[23] && counter < 5'd21) begin
+                mant_float = mant_float << 1'b1;
+                counter = counter + 1'b1;
+            end
+            exp_float = exp_float - counter;
+            result = {sign_float, exp_float, mant_float}
+        end
+    end
+
+endmodule
---
+++ Conversions/Float_Fixed_Conversion.v
@@ -0,0 +1,39 @@
+// single precision 32 bit (IEEE-754) to fixed point 22 bit. 
+// 22-bit fixed point : 1 sign bit + 1 integer bit + 20 fractional bits.
+
+module float_to_fixed(
+    data,
+    result,
+    clk
+    );
+
+    input clk;
+    input [31:0] data;
+    output reg [21:0] result;
+
+    wire sign_float;
+    wire [7:0] exp_float;
+    wire [22:0] mant_float;
+
+    reg sign_fixed;
+    reg [20:0] fixed_val;
+    reg [7:0] shifts;
+    reg [23:0] full_mant;
+
+    assign {sign_float, exp_float, mant_float[22:0]} = data;
+
+    always @ (posedge clk) begin
+        full_mant = {1'b1, mant_float};
+        sign_fixed = sign_float;
+
+        if (exp_float == 8'b0 || exp_floats > 8'd127) begin
+            result = 22'b0;
+        end
+        else begin
+            shifts = 8'd127 - exp_float;
+            full_mant = full_mant >> shifts;
+            fixed_val[20:0] = full_mant[23:3];
+            result = {sign_fixed, fixed_val[20:0]};
+        end
+    end
+endmodule
