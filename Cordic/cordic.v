4e52bb1badb9e0ef15eb33a24eb7746076e6a70f
Ed <harriss.edward24@gmail.com>
2021-03-21 11:57:48 +0000

    Done signal implmented

--- Cordic/cordic.v
+++ Cordic/cordic.v
@@ -3,24 +3,28 @@ module cordic(
     reset, //active-high
 	 start, //high at begg
     angle,
-    cos_out
+    cos_out,
+	 done
 );
 
 input clk, reset, start;
 input [21:0] angle;
 output [21:0] cos_out;
+output done;
 
-reg state, state_next;
+reg state, state_next, done_reg, done_next;
 reg [3:0] i, i_next;  //iterator
 reg [21:0] e_i, x, y, z, x_next, y_next, z_next; //angle of iteration plus others...
 
 wire [21:0] cos_out, x_shifted, y_shifted;
-wire d;
+wire d, done;
+
 
 assign cos_out = x;
 assign d = z[21];
 assign x_shifted = x >> i;
 assign y_shifted = y >> i;
+assign done = done_reg;
 
 always @(i) begin //angle to be used in calc
     case(i)
@@ -50,12 +54,14 @@ always@(posedge clk) begin
         y <= 22'd0;
         z <= angle;
         state <= 1'b1;
+		  done_reg <= 1'b0;
     end
 	 else if (reset) begin
         i <= 4'd0;
         x <= 22'b10011011011101001110;
         y <= 22'd0;
 		  z <= angle;
+		  done_reg <= 1'b0;
 	 end
     else begin        //updateing values
         x <= x_next;
@@ -63,6 +69,7 @@ always@(posedge clk) begin
         z <= z_next;
 		  i <= i_next;
         state <= state_next;
+		  done_reg <= done_next;
     end
 end
 
@@ -72,12 +79,14 @@ always @* begin
 	 z_next = z;
 	 i_next = i;
 	 state_next = state;
+	 done_next = done_reg;
     if(state)begin  //calculating section
         x_next = x + (d ? y_shifted : -y_shifted);
         y_next = y + (d ? -x_shifted : x_shifted);
         z_next = z + (d ? e_i : -e_i);
         i_next = i + 1;
         if(i == 4'd15) begin   //done 16 iterations
+				done_next = 1'b1;
             state_next = 1'b0; 
         end
     end
