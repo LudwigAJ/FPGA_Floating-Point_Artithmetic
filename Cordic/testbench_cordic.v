ee3f1303bfb3c1a356f39a67632eff9ba57e0fb7
Ed <harriss.edward24@gmail.com>
2021-03-21 19:05:05 +0000

    22 bit 4* unrolled

--- Cordic/testbench_cordic.v
+++ Cordic/testbench_cordic.v
@@ -2,19 +2,21 @@
 module tb ();
 
 	//Inputs
-	reg [31:0] angle;
+	reg [21:0] angle;
 	reg clk, reset, start;
 
 	//Output(s)
-	wire [31:0] cos_out;
-
+	wire [21:0] cos_out;
+	wire done;
+	
 	//module testing
-	cordic_unrolled unit(
+	cordic_unrolled_four unit(
 		.clk(clk),
 		.reset(reset), 
 		.start(start),
 		.angle(angle),
-		.cos_out(cos_out)
+		.cos_out(cos_out),
+		.done(done)
 	);
 
 
@@ -35,28 +37,28 @@ module tb ();
 		
 		#19
 		start = 1'b1;
-		angle <= 32'h20000000; //0.5
+		angle <= 22'h80000; //0.5
 		#2
 		start = 1'b0;
-		#18
+		#797
 		
 		start = 1'b1;
-		angle <= 32'h80000000;  //-1
-		#2
+		angle <= 32'h100000;  //-1
+		#4
 		start = 1'b0;
-		#18
-		
-		start = 1'b1;
-		angle <= 32'h60000000;  //1
-		#2
-		start = 1'b0;
-		#18
-		
-		start = 1'b1;
-		angle <= 32'h00000000;  //0
-		#2
-		start = 1'b0;
-		#18
+		#800
+//		
+//		start = 1'b1;
+//		angle <= 32'h60000000;  //1
+//		#2
+//		start = 1'b0;
+//		#18
+//		
+//		start = 1'b1;
+//		angle <= 32'h00000000;  //0
+//		#2
+//		start = 1'b0;
+//		#18
 			
 		$stop; //end of sim
 	end
