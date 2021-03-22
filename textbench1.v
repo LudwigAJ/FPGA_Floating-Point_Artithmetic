eca1b2b6b80a354de016acb5645d593e3527873d
Ludwig <ludwigalfred@gmail.com>
2021-03-22 16:42:10 +0000

    Update textbench1.v

--- textbench1.v
+++ textbench1.v
@@ -6,13 +6,30 @@ module tb ();
 	reg clk;
 	//Output from DUT is wire type
 	wire [31:0] result;
+	
+	wire [31:0] result1;
+	wire [31:0] result2;
+	wire [31:0] result3;
+	wire [31:0] result4;
+	wire [21:0] result5;
+	wire [21:0] result6;
+	wire [31:0] result7;
+	wire [31:0] result8;
 
 	//Instantiate the DUT
 	//mul refers to the verilog module defined by the LPM_MULT ip
 	Task7_Cordic_top unit(
 		.clk(clk),
 		.data(data), 
-		.result(result)
+		.result(result),
+		.result1(result1),
+		.result2(result2),
+		.result3(result3),
+		.result4(result4),
+		.result5(result5),
+	   .result6(result6),
+		.result7(result7),
+		.result8(result8)
 	);
 
 	// ---- If a clock is required, see below ----
