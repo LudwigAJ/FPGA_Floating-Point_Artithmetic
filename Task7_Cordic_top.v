686e4b90ff385f41438c957a222fe3bc93d93f5f
Ludwig <ludwigalfred@gmail.com>
2021-03-22 20:36:55 +0000

    Update Task7_Cordic_top.v

--- Task7_Cordic_top.v
+++ Task7_Cordic_top.v
@@ -13,41 +13,13 @@
 module Task7_Cordic_top(
     clk,
     data,
-	 result,
-    result1,
-	 result2,
-	 result3,
-	 result4,
-	 result5,
-	 result6,
-	 result7,
-	 result8,
-	 enable1,
-	 enable2,
-	 enable3,
-	 enable4,
-	 enable5,
-	 enable6,
-	 enable7,
-	 enable8,
-	 enable9
+    result,
     );
 
     input clk;
     input [31:0] data;
     output reg [31:0] result;
 	 
-	 output [31:0] result1;
-	 output [31:0] result2;
-	 output [31:0] result3;
-	 output [31:0] result4;
-	 output [21:0] result5;
-	 output [21:0] result6;
-	 output [31:0] result7;
-	 output [31:0] result8;
-	 
-	 output enable1, enable2, enable3, enable4, enable5, enable6, enable7, enable8, enable9;
-
     // Constants //
     //wire [31:0] point_five;
     //wire [31:0] one_twenty_eight;
@@ -81,8 +53,7 @@ module Task7_Cordic_top(
         .done(enable_1),
         .clk(clk)
         );
-	 assign result1 = result_first;
-	 assign enable1 = enable_1;
+
     Task6_Mult_top second_mult(
         .dataa(data),
         .datab(data),
@@ -91,8 +62,7 @@ module Task7_Cordic_top(
         .done(enable_2),
         .clk(clk)
         );
-	 assign result2 = result_second;
-	 assign enable2 = enable_2;
+
     Task6_Sub_top first_sub(
         .dataa(data),
         .datab(one_twenty_eight),
@@ -101,8 +71,7 @@ module Task7_Cordic_top(
         .done(enable_3),
         .clk(clk)
         );
-	 assign result3 = result_third;
-	 assign enable3 = enable_3;
+
     Task6_Mult_top third_mult(
         .dataa(result_third),
         .datab(one_over_one_twenty_eight),
@@ -111,8 +80,7 @@ module Task7_Cordic_top(
         .done(enable_4),
         .clk(clk)
         );
-	 assign result4 = result_fourth;
-	 assign enable4 = enable_4;
+
     Float_Fixed_Conversion floatToFixed(
         .data(result_fourth),
         .result(result_fourth_fixed),
@@ -120,8 +88,7 @@ module Task7_Cordic_top(
         .done(enable_5),
         .clk(clk)
     );
-	 assign result5 = result_fourth_fixed;
-	 assign enable5 = enable_5;
+
     reg geoff_reset = 1'b0;
 
     cordic geoff(
@@ -132,8 +99,7 @@ module Task7_Cordic_top(
         .cos_out(result_fifth),
 	    .done(enable_6)
     );
-	 assign result6 = result_fifth;
-	 assign enable6 = enable_6;
+
     Fixed_Float_Conversion fixedToFloat(
         .data(result_fifth),
         .result(result_fifth_fixed),
@@ -141,8 +107,7 @@ module Task7_Cordic_top(
         .done(enable_7),
         .clk(clk)
     );
-	 assign result7 = result_fifth_fixed;
-	 assign enable7 = enable_7;
+
     Task6_Mult_top fourth_mult(
         .dataa(result_second),
         .datab(result_fifth_fixed),
@@ -151,8 +116,7 @@ module Task7_Cordic_top(
         .done(enable_8),
         .clk(clk)
         );
-    assign result8 = result_sixth;
-	 assign enable8 = enable_8;
+
     Task6_Addr_top second_addr(
         .dataa(result_first),
         .datab(result_sixth),
@@ -161,7 +125,7 @@ module Task7_Cordic_top(
         .done(enable_9),
         .clk(clk)
         );
-    assign enable9 = enable_9;
+
     always @ (posedge clk) begin
         if (enable_9) begin
             result[31:0] <= result_seventh[31:0];
