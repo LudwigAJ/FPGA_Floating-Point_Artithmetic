e18243ab69a907bca5e6c3cafdae76f66247bd47
Ludwig <ludwigalfred@gmail.com>
2021-04-01 13:24:41 +0100

    Add files via upload

---
+++ extra_implementations/sum_and_input/testbench/Task8_Cordic_top.v
@@ -0,0 +1,69 @@
+module Task8_Cordic_top(
+    clk,
+    dataa, // the value to be calculated
+    datab, // the sum
+    result,
+    start,
+	 // For testbench //
+	 test_result_dataa,
+	 test_enable_dataa,
+	 test_enable_add
+	 // For testbench - end //
+    );
+
+		
+    input clk;
+    input start;
+    input [31:0] dataa, datab;
+    output reg [31:0] result;
+
+    wire [31:0] result_wire;
+
+    wire [31:0] result_dataa;
+    wire enable_dataa, enable_add;
+
+    reg [31:0] result_dataa_reg;
+    reg enable_dataa_reg, enable_add_reg;
+	 
+	 // For testbench //
+	 output [31:0] test_result_dataa;
+	 output test_enable_dataa;
+	 output test_enable_add;
+	 
+	 
+	 assign test_result_dataa = result_dataa_reg;
+	 assign test_enable_dataa = enable_add_reg;
+	 assign test_enable_add = enable_add_reg;
+	 
+	 // For testbench - end //
+
+
+    always @ (posedge clk) begin
+        if (enable_dataa) begin
+            result_dataa_reg <= result_dataa;
+            enable_add_reg <= 1'b1;
+        end
+        else if (enable_add) begin
+            enable_add_reg <= 1'b0;
+        end
+    end
+
+    Task8_Cordic_top_sub geoff(
+        .clk(clk),
+        .data(dataa),
+        .result(result_dataa),
+        .start(start),
+        .done(enable_dataa)
+        );
+       
+
+    Task6_Addr_top Last_adder(
+        .dataa(result_dataa_reg),
+        .datab(datab),
+        .result(result_wire),
+        .enable(enable_add_reg),
+        .done(enable_add),
+        .clk(clk)
+        );    
+
+endmodule
---
+++ extra_implementations/sum_and_input/testbench/testbench_1.v
@@ -0,0 +1,81 @@
+`timescale 1 ns / 100 ps
+module tb ();
+
+	//Inputs to DUT are reg type
+	reg [31:0] dataa;
+	reg [31:0] datab;
+	reg clk;
+	
+	reg start;
+	//Output from DUT is wire type
+	wire [31:0] result;
+	
+	wire [31:0] result_dataa;
+	wire [31:0] result_datab;
+
+	
+	wire enable_dataa;
+	wire enable_datab;
+	wire enable_add;
+
+	//Instantiate the DUT
+	//mul refers to the verilog module defined by the LPM_MULT ip
+	Task8_Cordic_top unit(
+		.clk(clk),
+		.dataa(dataa),
+		.datab(datab),
+		.result(result),
+		.start(start),
+		// for the testbench //
+		.test_result_dataa(result_dataa),
+		//.test_result_datab(result_datab),
+		.test_enable_dataa(enable_dataa),
+		//.test_enable_datab(enable_datab),
+		.test_enable_add(enable_add)
+		// for the testbench - end //
+    );
+
+	// ---- If a clock is required, see below ----
+	// //Create a 50MHz clock
+	always
+			#10 clk = ~clk;
+	// -----------------------
+
+	//Initial Block
+	initial
+	begin
+		$display($time, " << Starting Simulation >> ");
+		
+		// intialise/set input
+		 clk = 1'b0;
+		
+		// If using a clock
+		
+		
+		
+		@(posedge clk);
+	
+		dataa <= 32'b01000011011111110000000000000000;  // 255.0
+		datab <= 32'b01000011000000000000000000000000; // 128.0
+		
+		#20
+		
+		start <= 1'b1;
+		
+		#20
+		
+		start <= 1'b0;
+		
+		// Wait 10 cycles (corresponds to timescale at the top) 
+		
+		// set dataa and datab
+		
+		//datab <= 32'b0; 											// 0
+		
+		#1000
+		
+		$display($time, "<< Simulation Complete >>");
+		$stop;
+	end
+
+endmodule
